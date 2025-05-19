41f1c50441c6b5b5bc3e75dd458daa6e2f3dc2c0
Ubuntu <ubuntu@ip-172-31-40-76.eu-north-1.compute.internal>
2025-05-19 14:26:58 +0000

    Bug fixes

--- users/models.py
+++
@@ -1,1 +0,0 @@
-from users.app_models import *
--- users/models/LearningDomain.py
+++ users/models/LearningDomain.py
@@ -5,7 +5,7 @@ from parler.models import TranslatableModel, TranslatedFields
 
 class LearningDomain(TranslatableModel):
     icon = models.ImageField(upload_to='icon/', null=True, blank=True)
-    name = models.CharField(max_length=100, null=True, name='title')
+    title = models.CharField(max_length=100, null=True)
 
     translations = TranslatedFields(
         name_translated=models.CharField(_("Translated name"), max_length=100, null=True, blank=True), 
@@ -13,4 +13,4 @@ class LearningDomain(TranslatableModel):
 
     def __str__(self):
         translated_name = self.safe_translation_getter('name_translated', any_language=True)
-        return translated_name or self.name or _('Unnamed Domain')
+        return translated_name or self.title or _('Unnamed Domain')
